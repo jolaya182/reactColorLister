eb57d35df16296c6749cc2b465fe63488be2e392
javier olayis <jolaya182@gmail.com>
2018-01-23 23:54:03 -0500

    addedTopSemicolonsTAppjs

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
-import { Component } from 'react'
-import { v4 } from 'uuid'
-import AddColorForm from './AddColorForm'
-import ColorList from './ColorList'
-import '../../styleSheets/App.scss'
+import { Component } from 'react';
+import { v4 } from 'uuid';
+import AddColorForm from './AddColorForm';
+import ColorList from './ColorList';
+import '../../styleSheets/App.scss';
 
 export default class App extends Component {
 
