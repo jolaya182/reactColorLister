8e4655cf1945b007b547a0d6c056ddbad1691093
javier olayis <jolaya182@gmail.com>
2018-01-31 18:27:41 -0500

    addedSemicolonStarFile1

--- src/components/Star.js
+++ src/components/Star.js
@@ -1,5 +1,5 @@
-import PropTypes from 'prop-types'
-import '../../styleSheets/Star.scss'
+import PropTypes from 'prop-types';
+import '../../styleSheets/Star.scss';
 
 const Star = ({ selected=false, onClick=f=>f }) =>
     <div className={(selected) ? "star selected" : "star"}
