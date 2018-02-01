ca28d5f131a5a3b28448d1d3954662b99c40f7fe
javier olayis <jolaya182@gmail.com>
2018-02-01 11:07:21 -0500

    allExports
    addedSemicolonStarFile1

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -7,10 +7,10 @@ const StarRating = ({starsSelected=0, totalStars=5, onRate=f=>f}) =>
             <Star key={i}
                   selected={i<starsSelected}
                   onClick={() => onRate(i+1)}/>
-        )}
+        )} 
         <p>{starsSelected} of {totalStars} stars</p>
-    </div>
-
+    </div> 
+   
 StarRating.propTypes = {
     starsSelected: PropTypes.number,
     totalStars: PropTypes.number,
