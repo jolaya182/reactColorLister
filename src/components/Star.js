95f75e119bb7fa366b809e75fe866838b0d54d08
javier olayis <jolaya182@gmail.com>
2018-02-07 12:46:37 -0500

    needed to add

--- src/components/Star.js
+++ src/components/Star.js
@@ -1,14 +1,15 @@
 import PropTypes from 'prop-types';
 import '../../styleSheets/Star.scss';
 
-const Star = ({ selected=false, onClick=f=>f }) =>
-    <div className={(selected) ? "star selected" : "star"}
+const Star = ({ selected = false, onClick = f => f }) =>
+  (<div className={(selected) ? "star selected" : "star"}
          onClick={onClick}>
-    </div>
+    </div>);
 
 Star.propTypes = {
-    selected: PropTypes.bool,
-    onClick: PropTypes.func
-}
+  selected: PropTypes.bool,
+  onClick: PropTypes.func,
+};
 
 export default Star;
+// end of content
